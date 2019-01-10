1b75981ae9284a9418225f406a2efc95beed756a
jfed8 <jadenfeddock@gmail.com>
2019-01-10 15:51:25 -0800

    Initial Commit

---
+++ Rolex/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Rolex
+//
+//  Created by J J Feddock on 1/10/19.
+//  Copyright © 2019 HF Corporation. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Rolex/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Rolex
+//
+//  Created by J J Feddock on 1/10/19.
+//  Copyright © 2019 HF Corporation. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
